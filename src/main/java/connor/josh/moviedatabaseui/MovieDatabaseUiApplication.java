245a54e3fed30edd1d0e22f6582c6d07ede6726d
choffman4 <95241440+choffman4@users.noreply.github.com>
2022-11-10 14:43:47 -0700

    Initial commit

---
+++ src/main/java/connor/josh/moviedatabaseui/MovieDatabaseUiApplication.java
@@ -0,0 +1,13 @@
+package connor.josh.moviedatabaseui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabaseUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDatabaseUiApplication.class, args);
+    }
+
+}
