f3e6de35731fe93b79d09b915b99722f06b3cf27
choffman4 <95241440+choffman4@users.noreply.github.com>
2022-11-21 08:36:06 -0700

    added js pages

--- src/main/resources/static/javascript/sign_up.js
+++ src/main/resources/static/javascript/sign_up.js
@@ -7,24 +7,25 @@ var password = null;
 // const mysql = require("mysql");
 
 
-
+alert("hello")
 
 
 window.onload = function() {
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'test',
-        database: 'moviedb'
-    });
-    connection.connect((err) => {
-        if (err) throw err;
-        alert('Connected!');
-    });
-}
 
-
-function createUser() {
-    username = document.getElementById("username");
-    password = document.getElementById("password");
+    // const connection = mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: 'test',
+    //     database: 'moviedb'
+    // });
+    // connection.connect((err) => {
+    //     if (err) throw err;
+    //     alert('Connected!');
+    // });
 }
+//
+//
+// function createUser() {
+//     username = document.getElementById("username");
+//     password = document.getElementById("password");
+// }
