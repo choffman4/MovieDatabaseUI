917588f842da27ea13f3994c8d9108f910b055e0
Joshtuck017 <93556196+Joshtuck017@users.noreply.github.com>
2022-11-22 08:18:34 -0700

    Made basic move and user restcontrollers

--- src/main/java/connor/josh/moviedatabaseui/MovieBLL.java
+++ src/main/java/connor/josh/moviedatabaseui/MovieBLL.java
@@ -1,5 +1,8 @@
 package connor.josh.moviedatabaseui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,32 +19,5 @@ public class MovieBLL {
 
     public static ArrayList<Movie> allMovies = new ArrayList<>();
 
-    public List<Movie> findAll() throws IOException {
-        String sql = "Select * from moviedb.movie";
-        allMovies.clear();
-
-        try {
-            Connection con = DriverManager.getConnection(url, user, password);
-            PreparedStatement pst = con.prepareStatement(sql);
-            ResultSet rs = pst.executeQuery();
-
-            while(rs.next()) {
-                int id = rs.getInt("id");
-                String title = rs.getString("title");
-                String director = rs.getString("director");
-                int year = rs.getInt("year");
-                int runtime = rs.getInt("runtime");
-                float rating = rs.getFloat("rating");
-                int recommends = rs.getInt("recommends");
-                String imdbID = rs.getString("imdbID");
-                Movie movie = new Movie(id, title, director, year, runtime, rating, recommends, imdbID);
-                allMovies.add(movie);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return allMovies;
-    }
 
 }
--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -5,7 +5,6 @@ var currentPage = null;
 
 function fetchMovies(pageNumber) {
     currentPage = parseInt(pageNumber);
-    alert(currentPage)
     var movieList = document.getElementById("movieListHTML");
     movieList.innerHTML = "";
     fetch("http://www.omdbapi.com/?s=star&page=" + String(currentPage) + "&type=movie&apikey=7c9d77e9", {
@@ -39,7 +38,6 @@ function fetchMovies(pageNumber) {
 }
 
 function fetchMovie(imdbID) {
-    alert(imdbID)
     var movieList = document.getElementById("movieListHTML");
     movieList.innerHTML = "";
     fetch("http://www.omdbapi.com/?i=" + imdbID + "&plot=full&apikey=7c9d77e9", {
@@ -53,89 +51,3 @@ function fetchMovie(imdbID) {
             movieList.innerHTML += movieHTML;
         })
 }
-
-// for (var planet of planets) {
-//     var planet_html = "<hr/>Planet:<br/><li>" + "ID:&nbsp;" + planet.id + "</li>" + "<li>"
-//         + "NAME:&nbsp;" + planet.name + "</li>" + "<li>" + "SYSTEM:&nbsp;" + planet.system + "</li>"
-//         + "<li>" + "GALAXY:&nbsp;" + planet.galaxy + "</li>";
-//     planet_list.innerHTML += planet_html;
-// }
-
-////////////////////////////////////////////////////////////////////////////
-// function getAllMovies() {
-//     clearPage();
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function () {
-//         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-//             var allMovies = JSON.parse(this.responseText);
-//             renderMovies(allMovies);
-//         }
-//     }
-//     xmlHttp.open("GET", "http://localhost:8081/movies/", true);
-//     xmlHttp.send();
-// }
-
-// function searchPeople() {
-//     clearPage();
-//     var searchTerm = document.getElementById("entity_search").value;
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open("GET", "http://localhost:8081/movies/" + searchTerm, true);
-//     xmlHttp.onreadystatechange = function () {
-//         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-//             var people = JSON.parse(this.responseText);
-//             renderPeople(people);
-//         }
-//     }
-//     xmlHttp.send();
-// }
-
-// function renderMovies(allMovies) {
-//     for (var movie of allMovies) {
-//         var movie_html = "<hr/>Movie:<br/><li>" + "ID:&nbsp;" + movie.id + "</li>" + "<li>"
-//             + "Title:&nbsp;" + movie.title + "</li>" + "<li>" + "Director:&nbsp;" + movie.director + "</li>";
-//         movie_list.innerHTML += movie_html;
-//     }
-// }
-
-
-// function clearPage() {
-//     var movie_list = document.getElementById("movie_list");
-//     var movieHTML = document.getElementById("movieHTML");
-//     movie_list.innerHTML = "";
-//     movieHTML.innerHTML = "";
-// }
-//
-window.onload = function() {
-
-    // username = getCookie("username");
-    // password = getCookie("password");
-    // loadPeople();
-}
-
-// function loadPeople() {
-//     clearPage();
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function () {
-//         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-//             // var people = JSON.parse(this.responseText);
-//         }
-//     }
-//     xmlHttp.open("GET", "http://localhost:8081/movies/", true);
-//     xmlHttp.send();
-// }
-
-// function getCookie(cname) {
-//     let name = cname + "=";
-//     let decodedCookie = decodeURIComponent(document.cookie);
-//     let ca = decodedCookie.split(';');
-//     for(let i = 0; i <ca.length; i++) {
-//         let c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
